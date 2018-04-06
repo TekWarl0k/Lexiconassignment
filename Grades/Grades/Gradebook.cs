2cf899b89b456279017aceb4fdcd95d523836f26
Jens Andersson <jensandersson_88@hotmail.com>
2018-04-06 15:54:42 +0200

    Updates

--- Grades/Grades/Gradebook.cs
+++ Grades/Grades/Gradebook.cs
@@ -7,16 +7,37 @@ using System.Threading.Tasks;
 namespace Grades
 {
     class Gradebook
+        //En klass funkar som en mall för att göra objekt. Det kan också användas för att göra en typ som används för en variabel.
     {
         public Gradebook()
+            //Detta är en constructor, med den kan du bestämma vad ett objekt ska ha för standardvärden o.s.v.
         {
             grades = new List<float>();
+            //När objektet "grades" tillkallas så skapas en nya lista som kan ta emot float värden.
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum = sum + grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
         }
 
         public void AddGrade(float grade)
+        //Addgrade är en Method, float är typ och grade är en parameter.
         {
             grades.Add(grade);
         }
+        //Allt inom brackets kallas "Scope"
         private List<float> grades;
+        //Listan är en collection, som samlar float nummer. Fields "grades" är en variabel som deklarerats direkt i en class eller constructor.
     }
 }
--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -13,9 +13,13 @@ namespace Grades
             Gradebook book = new Gradebook();
             book.AddGrade(65);
             book.AddGrade(12);
+            book.AddGrade(44);
 
-            Gradebook book2 = book;
-            book2.AddGrade(10);
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+ 
         }
     }
 }
