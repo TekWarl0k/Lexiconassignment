3a3b72715c93365394b3700cc122b462cfa2b9d9
Jens Andersson <jensandersson_88@hotmail.com>
2018-04-06 11:40:15 +0200

    Exercise 2
    School Exercise 2

---
+++ exercise2/exercise2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime todaysDate = DateTime.Now;
+            DateTime tomorrowsDate = todaysDate.AddDays(1);
+            DateTime yesterdaysDate = todaysDate.AddDays(-1);
+
+            Console.WriteLine("Todays date is " +todaysDate);
+            Console.WriteLine("Tomorrows date is " + tomorrowsDate);
+            Console.WriteLine("Yesterdays date was " + yesterdaysDate);
+            Console.ReadKey();
+        }
+    }
+}
