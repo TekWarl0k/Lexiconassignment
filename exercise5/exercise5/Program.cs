0540419fdf24ae97a4d833472018f46b77727772
Jens Andersson <jensandersson_88@hotmail.com>
2018-04-12 15:53:10 +0200

    Beginning exercise 5
    Started an exercise.

---
+++ exercise5/exercise5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str = "Arrays are very common in programming, they look something like: [1,2,3,4,5]";
+            string VarArray;
+
+            int indexOfString = Str.IndexOf("[");
+            VarArray = Str.Remove(0, indexOfString);
+
+            Console.WriteLine(indexOfString);
+            Console.WriteLine(VarArray);
+        }
+    }
+}
