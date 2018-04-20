9b3400aa1f7f4357ee430ddba69a02969ad28ad3
Jens Andersson <jensandersson_88@hotmail.com>
2018-04-20 15:05:04 +0200

    Arena Fighter assignment
    Updated work on Arena Fighter.

--- arenafighter/arenafighter/Classes.cs
+++ arenafighter/arenafighter/Classes.cs
@@ -8,6 +8,8 @@ namespace arenafighter
 {
     public class Classes
     {
+        public bool Alive = true;
+
         public class Character
         {
             public string name;
@@ -18,36 +20,71 @@ namespace arenafighter
 
         public class Battle
         {
-            List<string> ListOfWins = new List<string>();
-            int points;
+            public List<string> ListOfWins = new List<string>();
+            public int points;
         }
 
         public class Round
         {
             //Character Hero = new Character();
             Random Dice = new Random();
-            public void CurrentRound(Character Hero, Character Enemy)
+            private bool Alive = true;
+
+            public void CurrentRound(Character Hero, Character Enemy, Battle Victory)
             {
-                int HeroDice = Hero.strength + Dice.Next(1, 6);
-                int EnemyDice = Enemy.strength + Dice.Next(1, 6);
-                if (HeroDice > EnemyDice)
+              while (Alive)
                 {
-                    Console.WriteLine("Rolls: " + Hero.name + " " + HeroDice + "vs " + Enemy.name + " " + EnemyDice);
-                    
-                    Console.WriteLine(Hero.name + "attacks " + Enemy.name + "! " + Enemy.name + " takes " + Hero.damage + " damage.");
-                    Console.WriteLine("Remaining Health " + Hero.health + " " + Enemy.health);
-                }
-                else if (HeroDice < EnemyDice)
-                {
-                    Console.WriteLine();
-                }
-                else if (HeroDice == EnemyDice)
-                {
-                    Console.WriteLine();
+                    int HeroDice = Hero.strength + Dice.Next(1, 6);
+                    int EnemyDice = Enemy.strength + Dice.Next(1, 6);
+                    if (HeroDice > EnemyDice)
+                    {
+                        Console.WriteLine("Rolls: " + Hero.name + " " + HeroDice + " vs " + Enemy.name + " " + EnemyDice);
+                        Console.WriteLine(Hero.name + " attacks " + Enemy.name + "! " + Enemy.name + " takes " + Hero.damage + " damage.");
+                        Enemy.health = Enemy.health - Hero.damage;
+                        Console.WriteLine("Remaining Health " + Hero.name + " " + Hero.health + " " + Enemy.name + " " + Enemy.health);
+                        Console.WriteLine();
+                    }
+                    else if (HeroDice < EnemyDice)
+                    {
+
+                        Console.WriteLine("Rolls: " + Hero.name + " " + HeroDice + " vs " + Enemy.name + " " + EnemyDice);
+                        Console.WriteLine(Enemy.name + " attacks " + Hero.name + "! " + Hero.name + " takes " + Enemy.damage + " damage.");
+                        Hero.health = Hero.health - Enemy.damage;
+                        Console.WriteLine("Remaining Health " + Hero.name + " " + Hero.health + " " + Enemy.name + " " + Enemy.health);
+                        Console.WriteLine();
+                    }
+                    else if (HeroDice == EnemyDice)
+                    {
+                        Console.WriteLine("Rolls: " + Hero.name + " " + HeroDice + " vs " + Enemy.name + " " + EnemyDice);
+                        Console.WriteLine("Neither finds any opening to attack and circle eachother searching.");
+                        Console.WriteLine();
+                    }
+                    if (Hero.health <= 0 || Enemy.health <= 0)
+                    {
+                        if (Hero.health <= 0)
+                        {
+                            Console.WriteLine(Enemy.name + " attacks " + Hero.name + "! " + Hero.name + " takes " + Enemy.damage + " damage and falls to the ground dead.");
+                            Alive = false;
+                            Victory.points = Victory.points + 1;
+                            Victory.ListOfWins.Add(Hero.name + " lost against " + Enemy.name);
+                            Console.WriteLine();
+                        }
+                        else if (Enemy.health <= 0)
+                        {
+                            Console.WriteLine(Hero.name + " attacks " + Enemy.name + "! " + Enemy.name + " takes " + Hero.damage + " damage and falls to the ground dead.");
+                            Alive = false;
+                            Victory.points = Victory.points + 2;
+                            Victory.ListOfWins.Add(Hero.name + " won against " + Enemy.name);
+                            Console.WriteLine();
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    Console.ReadKey();
                 }
             }
-            
-
             
             /*Dice roll to add to characters strength
              Compare strength and highest attacks
--- arenafighter/arenafighter/Program.cs
+++ arenafighter/arenafighter/Program.cs
@@ -12,30 +12,28 @@ namespace arenafighter
         static void Main(string[] args)
         {
             string[] EnemyName = {"Steve the Janitor", "Ivan the Lame", "Lisa the Picky", "Horrible Hank", "Non binary Brenda", "Booger eater Bob", "Charles Cow lover", "Jenny with the Axe"};
-            Random Stats = new Random();
 
+            Random Stats = new Random();
             Character Hero = new Character();
             Character Enemy = new Character();
+            Battle Victory = new Battle();
 
             bool isRunning = true;
             while (isRunning)
             {
+                Console.WriteLine("What is your name hero?");
+                Hero.name = Console.ReadLine();
+                Console.Clear();
 
                 Hero.strength = Stats.Next(3, 6);
                 Hero.damage = Stats.Next(2, 3);
                 Hero.health = Stats.Next(4, 7);
-                Round newRound = new Round();
-                newRound.CurrentRound(Hero, Enemy);
-
-
-                Console.WriteLine("What is your name hero?");
-                Hero.name = Console.ReadLine();
-                Console.Clear();
 
                 Console.WriteLine(Hero.name);
                 Console.WriteLine("Strength: " + Hero.strength);
                 Console.WriteLine("Damage: " + Hero.damage);
                 Console.WriteLine("Health: " + Hero.health);
+                Console.WriteLine();
 
                 while (isRunning)
                 {
@@ -46,6 +44,7 @@ namespace arenafighter
                     if (FightOrFlee == "F")
                     {
                         Console.Clear();
+                        Console.WriteLine("Player:");
                         Console.WriteLine(Hero.name);
                         Console.WriteLine("Strength: " + Hero.strength);
                         Console.WriteLine("Damage: " + Hero.damage);
@@ -58,19 +57,40 @@ namespace arenafighter
                         Enemy.damage = Stats.Next(2, 3);
                         Enemy.health = Stats.Next(4, 7);
 
+                        Console.WriteLine("Opponent:");
                         Console.WriteLine(Enemy.name);
                         Console.WriteLine("Strength " + Enemy.strength);
                         Console.WriteLine("Damage " + Enemy.damage);
-                        Console.WriteLine("health " + Enemy.health);
-                        
+                        Console.WriteLine("Health " + Enemy.health);
+                        Console.WriteLine();
+                        Console.ReadKey();
+
+                        Round newRound = new Round();
+                        newRound.CurrentRound(Hero, Enemy, Victory);
+
                     }
                     if (FightOrFlee == "R")
                     {
+                        Console.Clear();
+                        Console.WriteLine(Hero.name);
+                        Console.WriteLine("Strength: " + Hero.strength);
+                        Console.WriteLine("Damage: " + Hero.damage);
+                        Console.WriteLine("Health: " + Hero.health);
+                        Console.WriteLine();
+                        foreach (string win in Victory.ListOfWins)
+                        {
+                            Console.WriteLine(win);
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine(Victory.points);
+                        Console.ReadKey();
 
+                        isRunning = false;
                     }
                     /*Console.ReadKey();
                     Console.Clear();*/
                 }
+
             }
             
             
